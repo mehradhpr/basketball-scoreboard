be57c751fab6c54984b0abd640527b3409dd4df5
Mehrad Hassanpour <mehradhassanp@gmail.com>
2024-12-17 18:57:36 -0600

    Added two features: highlighting the winner and new gae button

--- script.js
+++ script.js
@@ -14,4 +14,30 @@ function increment(id) {
   } else {
     console.error("Error in the increment function")
   }
+
+  // updating the leader
+  if (homeCount == 0 && guestCount == 0) {
+    document.getElementById("h-section").style.borderColor = "transparent"
+    document.getElementById("g-section").style.borderColor = "transparent"
+  } else if (homeCount > guestCount) {
+    document.getElementById("h-section").style.borderColor = "yellow"
+    document.getElementById("g-section").style.borderColor = "transparent"
+  } else if (guestCount > homeCount) {
+    document.getElementById("h-section").style.borderColor = "transparent"
+    document.getElementById("g-section").style.borderColor = "yellow"
+  } else {
+    document.getElementById("h-section").style.borderColor = "black"
+    document.getElementById("g-section").style.borderColor = "black"
+  }
+
+}
+
+function newGame() {
+  homeCount = 0;
+  guestCount = 0;
+  document.getElementById("home-digit").textContent = 0
+  document.getElementById("guest-digit").textContent = 0
+
+  document.getElementById("h-section").style.borderColor = "transparent"
+  document.getElementById("g-section").style.borderColor = "transparent"
 }
